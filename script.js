ddc1fed5df267c13dfbb18824434a024d3494cc6
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2023-11-20 19:59:10 -0700

    Created index.html and script.js to start the RPS Game and .gitignore. Pseudocode for RPS game. .gitignore for .vscode workspace settings.

---
+++ script.js
@@ -0,0 +1,27 @@
+// Rock Paper Scissors
+// A simple rock paper scissors game vs a computer which "randomly"
+// picks one of the three options.
+// Pseudocode:
+// An "AI player" function which returns either Rock, Paper or Scissors - Randomly
+// A text input which accepts case-insensitve "rock", "paper" or "scissors"
+// A function which compares the two inputs and determine who the winner is based on the following conditions:
+//// 1. Rock beats Scissors
+//// 2. Scissors beats Paper
+//// 3. Paper beat Rock
+//// 4. All other results are a draw
+// Output who the winner is "Computer" or "Player"
+
+function getComputerChoice() {
+    
+
+
+}
+
+function playerSelection(string) {
+
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
