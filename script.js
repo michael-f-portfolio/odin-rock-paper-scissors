d77eb2e1004063e386e6a0b091d11635463652c4
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2023-11-21 16:38:53 -0700

    Added UI to RPS game, which includes a score tracker and declaration of a winner at 5 points.

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
+let playerScore = 0;
+let computerScore = 0;
+
 // Return either 1, 2 or 3.
 function getComputerChoice() {
     return Math.floor(Math.random() * (3)) + 1;
@@ -56,19 +59,53 @@ function playRound(playerSelection, computerSelection) {
     return winner;
 }
 
-function game(numberOfRounds) {
-    for (let i = 0; i < numberOfRounds; i++) {
-        let winner = playRound(getPlayerSelection(prompt()), getComputerChoice());
-        if (winner === 1) {
-            console.log("PLAYER WINS! :)");
-        } else if (winner === 2) {
-            console.log("CPU WINS! :(");
-        } else if (winner === 3) {
-            console.log("DRAW! :|");
-        } else if (winner === -1) {
-            console.log("Invalid input -- Please Try Again...");
-        }
+let rockBtn = document.querySelector("#rockBtn")
+rockBtn.addEventListener("click", function(e) {
+    let winner = playRound(getPlayerSelection("rock"), getComputerChoice());
+    getWinner(winner);
+
+});
+
+let paperBtn = document.querySelector("#paperBtn")
+paperBtn.addEventListener("click", function(e) {
+    let winner = playRound(getPlayerSelection("paper"), getComputerChoice());
+    getWinner(winner);
+
+});
+
+let scissorsBtn = document.querySelector("#scissorsBtn")
+scissorsBtn.addEventListener("click", function(e) {
+    let winner = playRound(getPlayerSelection("scissors"), getComputerChoice());
+    getWinner(winner);
+});
+
+function getWinner(winner) {
+    let showWinner = document.querySelector("#showWinner");
+    let playerScoreDisplay = document.querySelector("#playerScore");
+    let computerScoreDisplay = document.querySelector("#computerScore");
+
+    if (winner === 1) {
+        showWinner.textContent = "Player Wins!";
+        playerScore++;
+    } else if (winner === 2) {
+        showWinner.textContent = "Computer Wins!";
+        computerScore++;
+    } else if (winner === 3) {
+        showWinner.textContent = "Draw";
+    }
+
+    if (playerScore < 5 || computerScore < 5) {
+        playerScoreDisplay.textContent = `Player Score = ${playerScore}`;
+        computerScoreDisplay.textContent = `Computer Score = ${computerScore}`;
+    }
+
+    if (playerScore === 5) {
+        showWinner.textContent = "Congratulations you have won the game!";
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        showWinner.textContent = "Oh No! The Computer has won the game!";
+        playerScore = 0;
+        computerScore = 0;
     }
 }
-
-game(0);
